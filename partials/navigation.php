6139403f21fcb3fa7f10881c9d6fbfbcc0a20bfd
shllshckd <52105864+shllshckd@users.noreply.github.com>
2021-01-31 13:42:20 +0200

    padding, color improvements
    padding, color improvements

--- partials/navigation.php
+++ partials/navigation.php
@@ -3,12 +3,10 @@
         <img class="logo" src="static/logo.png">
         <span class="spanOne">Clock Hand Angle 2</span>
     </a>
-
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
             aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <i id="bars" class="fa fa-bars"></i>
     </button>
-
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav ml-auto">
             <li class="nav-item active">
